1d9f4826feed4f9b68a5e6ebf1cf32aa115a49fa
vanessaBonito <vanebonito@hotmail.com>
2022-12-30 09:58:58 -0500

    Create Headline component

---
+++ src/components/Headline/Headline.js
@@ -0,0 +1,24 @@
+const Headline = () => (
+    <section id="home-section" class="headline-section">
+    <div class="headline-description">
+        <div class="item-1">
+            <h1 class="text-titleH1 text-title item-1">Vanessa Bonito</h1>
+        </div>
+        <div class="item-2">
+            <p class="text-paragraph">
+                Hello! I am a software developer! I can help you build a product,
+                feature or website. Take a look of my works.
+                If you like what you see and have a project you need coded,
+                don’t hesitate and contact me.
+            </p>
+        </div>
+        <div class="item-3">
+            <button class="dark_button text-button" type="button"> Star collaboration</button>
+        </div>
+        <div class="description-decoration"></div>
+    </div>
+    <div class="headline-decoration"></div>
+</section>
+);
+
+export default Headline;
